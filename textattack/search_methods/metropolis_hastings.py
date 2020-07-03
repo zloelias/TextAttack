463d623e093a40c5299404bf62f52404fb64bc4a
Jin Yong Yoo <jy2ma@virginia.edu>
2020-07-03 12:41:05 -0400

    WIP: new sampling method

--- textattack/search_methods/metropolis_hastings.py
+++ textattack/search_methods/metropolis_hastings.py
@@ -1,13 +1,22 @@
+import copy
 import functools
 import math
 
 import numpy as np
 import torch
-from transformers import AutoModelWithLMHead, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 import textattack
+from textattack.goal_function_results import GoalFunctionResultStatus
 from textattack.search_methods import SearchMethod
 from textattack.shared import utils
+
+
+class PopulationMember:
+    def __init__(self, result, target_score):
+        self.result = result
+        self.target_score = target_score
+        self.alive = True
 
 
 class MetropolisHastingsSampling(SearchMethod):
@@ -20,15 +29,17 @@ class MetropolisHastingsSampling(SearchMethod):
             If the word count of the text under attack is greater than `max_iter`, we replace max_iter with word count for that specific example.
             This is so we at least try to perturb every word once. 
         lm_type (str): The language model to use to estimate likelihood of text.
-            Currently supported LM is "gpt-2"
+            Currently supported LM is "gpt2"
     """
 
-    def __init__(self, max_iter=200, lm_type="gpt2"):
+    def __init__(self, max_iter=50, pop_size=20, lm_type="gpt2"):
         self.max_iter = max_iter
+        self.pop_size = pop_size
         self.lm_type = lm_type
 
+        self._search_over = False
         self._lm_tokenizer = AutoTokenizer.from_pretrained(self.lm_type)
-        self._language_model = AutoModelWithLMHead.from_pretrained(self.lm_type)
+        self._language_model = AutoModelForCausalLM.from_pretrained(self.lm_type)
 
         try:
             # Try to use GPU, but sometimes we might have out-of-memory issue
@@ -57,15 +68,10 @@ class MetropolisHastingsSampling(SearchMethod):
         Returns: 1/perplexity(text)
         """
 
-        input_tokens = self._lm_tokenizer.tokenize(
-            text, max_length=self._lm_tokenizer.model_max_length - 2
-        )
-        # Occasionally, len(input_tokens) != 1022, so we have to check it
-        if len(input_tokens) != self._lm_tokenizer.model_max_length - 2:
-            input_tokens = input_tokens[: self._lm_tokenizer.model_max_length - 2]
-        input_tokens.insert(0, self._lm_tokenizer.bos_token)
-        input_tokens.append(self._lm_tokenizer.eos_token_id)
-        input_ids = self._lm_tokenizer.convert_tokens_to_ids(input_tokens)
+        input_ids = self._lm_tokenizer.encode(text)
+        input_ids = input_ids[:self._lm_tokenizer.model_max_length - 2]
+        input_ids.insert(0, self._lm_tokenizer.bos_token_id)
+        input_ids.append(self._lm_tokenizer.eos_token_id)
         input_ids = torch.tensor(input_ids)
         input_ids = input_ids.to(self._lm_device)
 
@@ -76,33 +82,20 @@ class MetropolisHastingsSampling(SearchMethod):
         perplexity = math.exp(loss)
         return 1 / perplexity
 
-    def _stationary_dist(self, x, original_output):
+    def _batch_target_prob(self, x_list):
         """
-        Unnormalized estimation of the distribution that we want to sample from.
+        Calculates unnormalized estimation of target_prob(x) in batch
+        target_prob(x) = lm_score(x) * prob_model(wrong|x)
         Args:
-            x (TokenizedText)
-            original_output
-        Returns: lm_score(x) * prob_model(wrong|x)
-        """
-        model_result = self.get_goal_results([x], original_output)[0][0]
-        lm_score = self._lm_score(x.text)
-        return lm_score * model_result.score
-
-    def _batch_stationary_dist(self, x_list, original_output):
-        """
-        Unnormalized estimation of the distribution that we want to sample from.
-        Process items in batch.
-        Args:
-            x_list (list): List of TokenizedText
-            original_output
-        Returns: list of floats representing lm_score(x) * prob_model(wrong|x)
+            attacked_text_list (list[AttackedText]): list of text we want to calculated probability of
+        Returns: float representing target_prob(x)
         """
         batch_size = len(x_list)
-        model_results = self.get_goal_results(x_list, original_output)[0]
+        goal_results, self._search_over = self.get_goal_results(x_list)
         lm_scores = [self._lm_score(x.text) for x in x_list]
-        scores = [lm_scores[i] * model_results[i].score for i in range(batch_size)]
+        scores = [lm_scores[i] * goal_results[i].score for i in range(batch_size)]
 
-        return scores, model_results
+        return scores, goal_results
 
     def _normalize(self, values):
         """
@@ -114,79 +107,125 @@ class MetropolisHastingsSampling(SearchMethod):
         else:
             return [v / s for v in values]
 
+    def _calc_return_prob(self, proposed_state, previous_state, index, initial_result):
+        # Now we have to calculate probability of return proposal g(x|x')
+        reverse_transformations = self.get_transformations(
+            proposed_state,
+            indices_to_modify=[index],
+            original_text=initial_result.attacked_text,
+        )
+
+        reverse_found = False
+        for t in reverse_transformations:
+            if t.text == previous_state.text:
+                reverse_found == True
+                break
+
+        if reverse_found:
+            return 1 / len(reverse_transformations)
+        else:
+            return 0.0
+
     def _perform_search(self, initial_result):
         text_len = len(initial_result.attacked_text.words)
         max_iter = max(self.max_iter, text_len)
 
-        current_result = initial_result
-        current_text = initial_result.attacked_text
-        current_score = self._stationary_dist(current_text, initial_result.output)
+        # target_score = LM(initial_text) * p(y_c | x_orig)
+        initial_target_score = (
+            self._lm_score(initial_result.attacked_text.text) * initial_result.score
+        )
+        initial_pop_member = PopulationMember(initial_result, initial_target_score)
+        population = [copy.deepcopy(initial_pop_member) for _ in range(self.pop_size)]
 
-        for n in range(max_iter):
-            # i-th word we want to transform
-            i = n % text_len
-
-            transformations = self.get_transformations(
-                current_text,
-                indices_to_modify=[i],
-                original_text=initial_result.attacked_text,
-            )
-
-            if len(transformations) == 0:
-                continue
-
-            scores, model_results = self._batch_stationary_dist(
-                transformations, initial_result.output
-            )
-            proposal_dist = self._normalize(scores)
-            # Choose one transformation randomly according to proposal distribution
-            jump = np.random.choice(list(range(len(transformations))), p=proposal_dist)
-
-            # Now we have calculate probability of return proposal g(x'|x)
-            reverse_transformations = self.get_transformations(
-                transformations[jump],
-                indices_to_modify=[i],
-                original_text=initial_result.attacked_text,
-            )
-
-            reverse_jump = None
-            for k in range(len(reverse_transformations)):
-                if reverse_transformations[k].text == current_text.text:
-                    # Transition x -> x' exists
-                    reverse_jump = k
-                    break
-            if not reverse_jump:
-                return_prob = 0
-            else:
-                ret_scores, _ = self._batch_stationary_dist(
-                    reverse_transformations, initial_result.output
+        for _ in range(max_iter):
+            for k in range(len(population)):
+                modifiable_indices = (
+                    set(range(text_len))
+                    - population[k].result.attacked_text.attack_attrs[
+                        "modified_indices"
+                    ]
                 )
-                return_prob = self._normalize(ret_scores)[reverse_jump]
+                while modifiable_indices:
+                    rand_idx = np.random.choice(tuple(modifiable_indices))
+                    transformations = self.get_transformations(
+                        population[k].result.attacked_text,
+                        indices_to_modify=[rand_idx],
+                        original_text=initial_result.attacked_text,
+                    )
 
-            """
-            According to Metropolis-Hastings algorithm
-            let f(x) be value proportional to target distribution p(x)
-            and g(x|x') be transition probability from x' to x.
-            Then, acceptance ratio = min(1, (f(x')*g(x|x')) / (f(x) * g(x'|x)))
-            """
-            if current_score == 0.0:
-                current_score += 1e-9
-            acceptance_ratio = (scores[jump] * return_prob) / (
-                current_score * proposal_dist[jump]
-            )
-            acceptance_ratio = min(1, acceptance_ratio)
-            u = np.random.uniform(low=0.0, high=1.0)
+                    if len(transformations) > 0:
+                        break
+                    else:
+                        modifiable_indices.remove(rand_idx)
 
-            if acceptance_ratio >= u or model_results[jump].succeeded:
-                # Accept the proposed jump
-                current_result = model_results[jump]
-                current_text = transformations[jump]
-                current_score = scores[jump]
+                if not modifiable_indices:
+                    # No transformations can be generated
+                    # Kill this population member and repopulate
+                    population[k].alive = False
+                    continue
 
-            if current_result.succeeded:
+                target_scores, goal_results = self._batch_target_prob(transformations)
+
+                if self._search_over:
+                    break
+
+                proposal_prob = 1 / len(transformations)
+                # Select on transformation uniformly-random
+                jump = np.random.choice(list(range(len(target_scores))))
+                return_prob = self._calc_return_prob(
+                    transformations[jump],
+                    population[k].result.attacked_text,
+                    rand_idx,
+                    initial_result,
+                )
+
+                """
+                According to Metropolis-Hastings algorithm
+                let f(x) be value proportional to target distribution p(x)
+                and g(x|x') be transition probability from x' to x.
+                Then, acceptance ratio = min(1, (f(x')*g(x|x')) / (f(x) * g(x'|x)))
+                """
+                """
+                acceptance_score = (target_scores[jump] * return_prob) / (
+                    population[k].target_score * proposal_prob
+                )
+                """
+                acceptance_score = target_scores[jump] / population[k].target_score
+                acceptance_score = min(1, acceptance_score)
+                u = np.random.uniform(low=0.0, high=1.0)
+                if False:
+                    print(f"target_score: {target_scores[jump]}")
+                    print(f"return_prob: {return_prob}")
+                    print(f"prev_score: {population[k].target_score}")
+                    print(f"proposal_prob: {proposal_prob}")
+                if (
+                    acceptance_score >= u
+                    or goal_results[jump].goal_status
+                    == GoalFunctionResultStatus.SUCCEEDED
+                ):
+                    # Accept the proposed jump
+                    population[k].result = goal_results[jump]
+                    population[k].target_score = target_scores[jump]
+
+            if self._search_over:
                 break
 
-        return current_result
+            for k in range(len(population)):
+                if not population[k].alive:
+                    # Repopulate using original text
+                    # TODO consider other way to repopulate?
+                    population[k] = copy.deepcopy(initial_pop_member)
+
+            population = sorted(population, key=lambda x: x.result.score, reverse=True)
+
+            if (
+                self._search_over
+                or population[0].result.goal_status
+                == GoalFunctionResultStatus.SUCCEEDED
+            ):
+                break
+
+        return population[0].result
 
     def extra_repr_keys(self):
-        return ["max_iter", "lm_type"]
+        return ["max_iter", "pop_size", "lm_type"]
